fefd2e617e43650bb708b60f21633607e663b3ee
ramed rafie <marf4196@outlook.com>
2022-08-28 15:24:10 +0430

    templates and static file for development added in settings

--- core/core/settings.py
+++ core/core/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['project/templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'project/static/'
 STATIC_ROOT = BASE_DIR / 'static/'
 
 STATICFILES_DIRS = [
